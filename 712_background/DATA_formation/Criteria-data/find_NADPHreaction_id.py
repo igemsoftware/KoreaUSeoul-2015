62d464c9b7524f6e46da7ea02254c0c8eace64e6
Junhee Kim <KoreaUSeoul2015@gmail.com>
2015-09-16 18:07:11 +0900

    Update find_NADPHreaction_id.py

--- 712_background/DATA_formation/Criteria-data/find_NADPHreaction_id.py
+++ 712_background/DATA_formation/Criteria-data/find_NADPHreaction_id.py
@@ -1,1 +1,41 @@
+## !/usr/bin/python
+## function:
+## input:
+## output:
 
+## import modules
+
+import itertools
+import networkx as nx
+import json
+import urllib2, sys, re
+from collections import defaultdict
+ 
+## define the function#1 to download the information
+
+def download_file(path):
+	data = urllib2.urlopen(path, timeout = 200).read()
+	data_split = data.split('\n')
+
+	return data_split
+
+
+## what is your goal(G)?
+G = 'C00005'
+goal = 'NADPH'
+
+
+### get the reactions concerning NADPH and ADP and save it
+ 
+NADPH_rn_file = 'http://rest.kegg.jp/link/reaction/C00005'
+NADPH_rn = download_file(NADPH_rn_file)
+ 
+output_file = 'NADPH_reaction_id.txt'
+output = open(output_file ,'w')
+ 
+for line in NADPH_rn:
+        if re.search(r'rn:(R\d+)',line):
+                rn = re.findall(r'rn:(R\d+)',line)
+                Rn = ''.join(rn)
+                output.write('%s\n' % (Rn))
+output.close()

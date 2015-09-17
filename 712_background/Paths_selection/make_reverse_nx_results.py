f862bc830293aa23b31625d27c2db671b9f53871
Junhee Kim <KoreaUSeoul2015@gmail.com>
2015-09-17 21:39:27 +0900

    Update and rename make_reverse_nx_result.py to make_reverse_nx_results.py

--- 712_background/Paths_selection/make_reverse_nx_results.py
+++ 712_background/Paths_selection/make_reverse_nx_results.py
@@ -1,14 +1,13 @@
 ## !/usr/bin/python
-## function:
-## input:
-## output:
+## function: produce the file without paths that make errors due to their overlapping R#####
+## output: The output file name that you want (we recommend the input name to be added ‘_reversed’)
 
 ### Import modlules
 import sys, re, os
 from optparse import OptionParser
 
 def main(argv):
-	optparse_usage = 'make_reverse_nx_result.py -i <input_path> -o <ouput_name> -r <root_dir>'
+	optparse_usage = 'make_reverse_nx_results.py -i <input_path> -o <output_name> -r <root_dir>'
         parser = OptionParser(usage=optparse_usage)
         parser.add_option("-i", "--inputpath", action="store", type="string",
                 dest="input_path", help='The input text file of top3 paths of all compound pairs')
@@ -34,8 +33,8 @@ def main(argv):
 		print 'Error : please write output file name that you want'
 	if len(args) > 0:
 		output_name += args
- 
-        make_file(input_path, output_name)
+	# Run the function
+        make_file(input_path, output_name, root_dir)
  
 
 
@@ -46,10 +45,10 @@ def read_file(input_file):
 		txt = list(line for line in txt if line)
 	return txt
 
-def make_file(input_path, output_name):
+def make_file(input_path, output_name, root_dir):
 	nt_result_txt = read_file(input_path)
 	output = ''.join(output_name)
-	output_file = '%s' %(output)
+	output_file = '%s/%s' %(root_dir, output)
 	output_handle = open(output_file, 'w')
 	i = 1
 	for line in nt_result_txt:

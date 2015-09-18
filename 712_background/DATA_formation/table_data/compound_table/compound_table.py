8518236738cafd7fd47d60d19e3e6894a25c2e5f
Junhee Kim <KoreaUSeoul2015@gmail.com>
2015-09-19 01:04:30 +0900

    Update compound_table.py

--- 712_background/DATA_formation/table_data/compound_table/compound_table.py
+++ 712_background/DATA_formation/table_data/compound_table/compound_table.py
@@ -1,104 +1,91 @@
-import urllib2
-import sys, re
+import sys, re, os
+from optparse import OptionParser
 
-database = open('/home/dhkim/table_making/compound_id.txt', 'r')
-for names in database:
-    mainbody = 'http://www.kegg.jp/dbget-bin/www_bget?'
-    tails = names
-    adress = '%s%s' % (mainbody, tails)
-
-    f = urllib2.urlopen(adress)
-
-    chemical_name = ''
-    formula = ''
-    exact_mass = ''
-    mol_weight = ''
-    i = 1
-    k = 0
-    for line in f:
-        if i == 157:
-            line_split = line.split('>')
-            chemical_name = '%s%s%s' % ('<td>', line_split[3][:-3], '</td>')
-            i += 1
-        elif i == 158 and line.rstrip() != '</div></div></td></tr>':
-            i += 1
-            k += 1
-        elif i == 159 and line.rstrip() != '</div></div></td></tr>' and k == 1:
-            i += 1
-            k += 1
-        elif i == 160 and line.rstrip() != '</div></div></td></tr>' and k == 2:
-            i += 1
-            k += 1
-        elif i == 161 and line.rstrip() != '</div></div></td></tr>' and k == 3:
-            i += 1
-            k += 1
-        elif i == 162 and line.rstrip() != '</div></div></td></tr>' and k == 4:
-            i += 1
-            k += 1
-        elif i == 163 and line.rstrip() != '</div></div></td></tr>' and k == 5:
-            i += 1
-            k += 1
-        elif i == 164 and line.rstrip() != '</div></div></td></tr>' and k == 6:
-            i += 1
-            k += 1
-        elif i == 165 and line.rstrip() != '</div></div></td></tr>' and k == 7:
-            i += 1
-            k += 1
-        elif i == 166 and line.rstrip() != '</div></div></td></tr>' and k == 8:
-            i += 1
-            k += 1
-        elif i == 162 + int(k):
-            line_split = line.split('>')
-            formula = '%s%s%s' % ('<td>', line_split[2][:-3], '</td>')
-            i += 1
-        elif i == 167 + int(k):
-            line_split = line.split('>') 
-            if line_split[2] != '<br':
-                exact_mass = '%s%s%s' % ('<td>', line_split[2][:-3], '</td>')
-                i += 1
-            else :
-                exact_mass = '%s' % ('<td>No data</td>')
-                i += 1
-        elif i == 172 + int(k):
-            line_split = line.split('>')
-            if line_split[2] != '<br':
-                mol_weight = '%s%s%s' % ('<td>', line_split[2][:-3], '</td>')
-                i += 1
-            else :
-                mol_weight = '%s' % ('<td>No data</td>')
-                i += 1
+def main(argv):
+        optparse_usage = 'compound_table.py -i <input_paths> -f <table_format> -r <root_dir>'
+        parser = OptionParser(usage=optparse_usage)
+        parser.add_option("-i", "--inputpaths", action="store", type="string",
+                dest="input_path", help='The input file is the kegg_compound_part.tsv')
+        parser.add_option("-r", "--rootDir", action="store", type="string",
+                dest="root_dir", help='The root directory. All files are generated here.')
+        parser.add_option("-f", "--tableFormat", action="store", type="string",
+                dest="format_path", help='The different number that you have given before.')
+	
+	(options, args) = parser.parse_args()
+	if options.input_path:
+                input_path = os.path.abspath(options.input_path)
         else:
-            i += 1
-
-    table_format = open('/home/dhkim/table_making/table_format.txt', 'r')
-    final = ''
-    n1 = '<td class="tg-x0js"> <image src="http://www.genome.jp/Fig/compound/'
-    n2 = '.gif" width="150" height="100" ></td>'
-    img = '%s%s%s' % (n1, tails.rstrip(), n2)
-    j = 1
-    for sets in table_format:
-        if j == 6:
-            final += '%s%s%s' % ('<caption>', tails.rstrip(), '</caption>')
-            j += 1
-        elif j == 9:
-            final += chemical_name
-            j += 1
-        elif j == 13:
-            final += formula
-            j += 1
-        elif j == 17:
-            final += exact_mass
-            j += 1
-        elif j == 21:
-            final += mol_weight
-            j += 1
-        elif j == 25:
-            final += img
-            j += 1
+                print 'Error: please provide proper input file name'
+	if options.format_path:
+                format_path = os.path.abspath(options.format_path)
         else:
-            final += sets
-            j += 1
-    ff = '%s.html' % (tails.rstrip())
-    output = open(ff, 'w')
-    output.write('%s' % (final)) 
-    output.close()
+                print 'Error: please provide proper input file name'
+	if options.root_dir:
+                root_dir = os.path.abspath(options.root_dir)
+        else:
+                print 'ERROR: please provide proper root direcotory'
+	# Run functions
+	print "working"
+	make_file(input_path, format_path, root_dir)
+
+
+def read_file(input_file):
+        with open(input_file) as f_in:
+                txt = (line.rstrip() for line in f_in)
+                txt = list(line for line in txt if line)
+        return txt
+	
+def make_file(input_path, format_path, root_dir):
+	database = read_file(input_path)
+	for lines in database:
+    		raw = re.split('\t', lines)
+		print raw
+    		name = raw[1].split(';///')
+    		chemical_name = '%s%s%s' % ('<td><center>', name[0], '</center></td>')
+    		if 3 >= len(raw):
+        		formula = '<td><center>No data</center></td>'
+    		else:
+        		formula = '%s%s%s' % ('<td><center>', raw[2], '</center></td>')
+    		if 4 >= len(raw) :
+        		exact_mass = '<td><center>No data</center></td>'
+    		else:
+        		exact_mass = '%s%s%s' % ('<td><center>', raw[3], '</center></td>')
+    		if 5 >= len(raw) :
+        		mol_weight = '<td><center>No data</center></td>'
+    		else:
+        		mol_weight = '%s%s%s' % ('<td><center>', raw[4], '</center></td>')
+    		table_format = read_file(format_path)
+    		final = ''
+    		n1 = '<td class="tg-x0js"> <image src="http://www.genome.jp/Fig/compound/'
+    		n2 = '.gif" width="150" height="100" ></td>'
+    		img = '%s%s%s' % (n1, raw[0], n2)
+    		j = 1
+    		for sets in table_format:
+        		if j == 6:
+            			final += '%s%s%s' % ('<caption>', raw[0], '</caption>')
+            			j += 1
+        		elif j == 9:
+            			final += chemical_name
+            			j += 1
+        		elif j == 13:
+            			final += formula
+            			j += 1
+        		elif j == 17:
+            			final += exact_mass
+            			j += 1
+        		elif j == 21:
+            			final += mol_weight
+            			j += 1
+        		elif j == 25:
+            			final += img
+            			j += 1
+        		else:
+            			final += sets
+            			j += 1
+    			ff = '%s/%s.html' % (root_dir, raw[0])
+    			output = open(ff, 'w')
+    			output.write('%s' % (final))
+    			output.close()
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
